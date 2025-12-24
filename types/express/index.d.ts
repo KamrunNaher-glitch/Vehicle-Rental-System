6200c88f9a018830a873321b930b0da1bfd72371
KamrunNaher-glitch <keyachandani7@gmail.com>
2025-12-24 23:20:31 +0600

    authentication and authorization

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -1,15 +1,9 @@
+import { express } from "express";
 import { JwtPayload } from "jsonwebtoken";
-
 declare global {
   namespace Express {
     interface Request {
-      user?: {
-        id: string;
-        role: string;
-        email?: string;
-      };
+      user?: JwtPayload;
     }
   }
 }
-
-export {};
